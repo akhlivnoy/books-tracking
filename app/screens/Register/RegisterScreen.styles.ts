7a89659a6573c0c7537198e143bab75b85941098
Andrii Khlivnyi <akhlivnoy@gmail.com>
2022-08-12 14:55:05 +0300

    feat(register-screen): RegisterScreen skeleton

---
+++ app/screens/Register/RegisterScreen.styles.ts
@@ -0,0 +1,67 @@
+import { StyleSheet, ViewStyle, TextStyle, Dimensions } from 'react-native';
+
+import { COLORS } from '../../themes/colors';
+
+interface IHomeScreenStyles {
+  main: ViewStyle;
+  background: ViewStyle;
+  contentContainer: ViewStyle;
+  titleContainer: ViewStyle;
+  title: TextStyle;
+  darkgreyColor: TextStyle;
+  input: ViewStyle;
+  bottomContainer: ViewStyle;
+  forestmistColor: TextStyle;
+}
+
+export const styles = StyleSheet.create<IHomeScreenStyles>({
+  main: {
+    flex: 1,
+    paddingHorizontal: 25,
+    paddingVertical: 100,
+    backgroundColor: COLORS.TREE,
+  },
+  background: {
+    position: 'absolute',
+    zIndex: 1,
+    bottom: 0,
+    left: 0,
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height * 0.72,
+    backgroundColor: COLORS.FOG,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  contentContainer: {
+    flex: 1,
+    zIndex: 2,
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 20,
+    paddingVertical: 50,
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 20,
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginBottom: 35,
+  },
+  title: {
+    marginBottom: 4,
+    color: COLORS.TREE,
+  },
+  darkgreyColor: {
+    color: COLORS.DARKGREY,
+  },
+  input: {
+    marginBottom: 15,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 15,
+  },
+  forestmistColor: {
+    color: COLORS.FORESTMIST,
+    textDecorationLine: 'underline',
+  },
+});
